9e63efd6a370594a0cd2b312409133f76190aead
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-07-13 13:55:16 +0300

    зробив бургер меню

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,27 +1,12 @@
-const openBurgerBtn = document.querySelector('.menu__burger-icon')
-const closedBurgerBtn = document.querySelector('.menu__burger-closed')
-const menuBurger = document.querySelector('.menu__burger')
+const BurgerBtn = document.querySelector('.burger__btn')
+const menuBurger = document.querySelector('.burger__container')
 const body = document.querySelector('body')
-const eventsClose = document.querySelectorAll('.menu__burger-link')
-
-openBurgerBtn.addEventListener('click', onOpenBurger)
-closedBurgerBtn.addEventListener('click', onClosedBurger)
-
-function onOpenBurger (){
-    menuBurger.classList.add('active__burger')
-    body.classList.add('hidden')
-}
-
-function onClosedBurger (){
-    menuBurger.classList.remove('active__burger')
-    body.classList.remove('hidden')
-}
 
 
-for (let eventClose of eventsClose) {
-    eventClose.addEventListener('click', onCloseModalBurger);
-  }
-function onCloseModalBurger (){
-    menuBurger.classList.remove('active__burger')
-    body.classList.remove('hidden')
+BurgerBtn.addEventListener('click', onToggleMenuBurger)
+
+
+function onToggleMenuBurger (){
+    menuBurger.classList.toggle('active__burger')
+    body.classList.toggle('hidden')
 }
