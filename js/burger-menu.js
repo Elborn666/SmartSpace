628cbe297e22dbccad9f2bb733bfc349086b253a
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-07-17 12:03:21 +0300

    відкорегував бургер меню

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,7 +1,7 @@
 const BurgerBtn = document.querySelector('.burger__btn')
 const menuBurger = document.querySelector('.burger__container')
 const body = document.querySelector('body')
-
+let windowWidth = document.documentElement.clientWidth
 
 BurgerBtn.addEventListener('click', onToggleMenuBurger)
 
@@ -10,3 +10,19 @@ function onToggleMenuBurger (){
     menuBurger.classList.toggle('active__burger')
     body.classList.toggle('hidden')
 }
+
+// Слухач який реагує на зміну ширини екрана
+
+window.addEventListener("resize",  onRemoveBlock)
+
+function onRemoveBlock () {
+    if(document.documentElement.clientWidth > 600){
+       console.log('width > 600')
+       menuBurger.classList.remove('active__burger')
+       body.classList.remove('hidden')
+    } else {
+        console.log('width < 600')
+    }
+}
+
+onRemoveBlock()
